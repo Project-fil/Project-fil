24b393beeafa42ef35ccc04fc1dcf68ff40fc6f6
Boyko Nikolay <enzeox@gmail.com>
2021-03-05 05:25:59 +0200

    Для наглядности накинул таблицу

--- src/com/github/app/InformationView.java
+++ src/com/github/app/InformationView.java
@@ -11,7 +11,13 @@ public class InformationView extends JPanel {
         setVisible(true);
         setBorder(BorderFactory.createLineBorder(Color.BLACK));
 
-        JScrollPane scrollPane = new JScrollPane();
+        String[] columnNames = {"id", "firstname", "lastname", "age", "city"};
+        String[][] data = {{"1", "kuk", "skuk", "23", "Megakuk"},
+                {"2", "look", "chesnok", "23", "Balalayka"}};
+        JTable informationTable = new JTable(data, columnNames);
+        informationTable.setBounds(5,5,540,300);
+
+        JScrollPane scrollPane = new JScrollPane(informationTable);
         scrollPane.setBounds(5,5,590,590);
         add(scrollPane);
     }
